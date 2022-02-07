d9ffe89c7c619d7f5508e66d060f532024571f8f
Norton29 <norton290194@gmail.com>
2022-02-07 19:09:08 -0300

    sqlDados

--- src/main/resources/META-INF/sql/load-script.sql
+++ src/main/resources/META-INF/sql/load-script.sql
@@ -15,5 +15,7 @@ INSERT INTO PUBLIC.ProfileMenu (profileId, menuId) VALUES (3, 1);
 INSERT INTO PUBLIC.ProfileMenu (profileId, menuId) VALUES (3, 2);
 
 INSERT INTO PUBLIC.Person (id, fullName, document, birthDate, createdAt) VALUES (1, 'Albert Edmélio Viana Costa', '04225778126', '1994-02-01', CURRENT_TIMESTAMP());
+INSERT INTO PUBLIC.Person (id, fullName, document, birthDate, createdAt) VALUES (2, 'Norton Ferreira Fernandes', '04070733108', '1994-01-29', CURRENT_TIMESTAMP());
 
 INSERT INTO PUBLIC.User_ (id, login, password, profileId, personId) VALUES (1, '04225778126', '123456789', 1, 1);
+INSERT INTO PUBLIC.User_ (id, login, password, profileId, personId) VALUES (2, '04070733108', '123456789', 2, 2);
